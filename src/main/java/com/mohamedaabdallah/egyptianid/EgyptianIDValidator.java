189e2fb3e5f97eb6cfc3b5dc625bc55593a48748
Mohamed A. Abdallah <Eng.Mohamed.A.Abdallah@gmail.com>
2025-06-16 00:01:18 +0300

    Initialize project structure

---
+++ src/main/java/com/mohamedaabdallah/egyptianid/EgyptianIDValidator.java
@@ -0,0 +1,10 @@
+package com.mohamedaabdallah.egyptianid;
+
+import com.mohamedaabdallah.egyptianid.components.*;
+import com.mohamedaabdallah.egyptianid.internal.*;
+
+public class EgyptianIDValidator {
+    public static void validate(String id) {
+        // TODO: Integrate format check, checksum, component parsing
+    }
+}
